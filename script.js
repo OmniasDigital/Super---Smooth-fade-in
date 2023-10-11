6518aa48c99c22b94deb90ac0e8e16f9aee7070f
Omnias Digital <108580191+OmniasDigital@users.noreply.github.com>
2023-10-10 21:02:50 -0400

    Added comments divs wont loose show class after unobserve

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//get all direct childs from a parent div and add a opacity of 0 to each
 pageDivs = document.querySelectorAll('.wrapper > *');
 pageDivs.forEach(div => {
     div.style.opacity = "0"
@@ -5,14 +6,20 @@ pageDivs.forEach(div => {
 
 const observer = new IntersectionObserver ( entries => {
     entries.forEach(entry => {
-        entry.target.classList.toggle("show", entry.isIntersecting)
+
+        //adds "show" class to currently intersecting
+        entry.target.classList.toggle("show", entry.isIntersecting) 
+
+        //once Intersected wont remove class
+        if (entry.isIntersecting) observer.unobserve(entry.target) 
     })
     },
     {
-        threshold: .1,
+        threshold: .4,
     }
 )
 
+//runs observer on search found div
 pageDivs.forEach((div) => {
     observer.observe(div)
 })
